2925d3b14da77c6f853878a17bf5ae8419a9f708
Lucas-far <mizoguchia@hotmail.com>
2023-03-13 12:01:43 -0300

    editing files

--- sintaxes_p_pa_models/fields.py
+++ sintaxes_p_pa_models/fields.py
@@ -1,11 +1,44 @@
 
 
+def parametros_padrao():
+    """
+    OBS       || Todos os campos de modelo Django possuem 2 parâmetros padrão
+    par_1     || String p/ explicar a descrição do campo no template admin (rótulo do input)
+    help_text || String p/ instrução (exemplo, detalha extra sobre o campo)
+    """
+
+
+class BooleanField:
+    """
+    default || Configura um checkbox p/ informar a disponibilidade de um objeto no template admin
+    sintaxe || models.BooleanField('Disponibilidade', help_text='', default=True)
+
+    * ===== AUTOMATIZAR CONTROLE DE QUANTIDADE =====
+    * Supondo que o objeto seja um produto e ele possui um estoque
+    * Uma função que poderia controlar/monitorar isso
+
+    def save(self, *args, **kwargs):
+        if self.storage > 0:
+            self.availability = True
+        if self._integer <= 0:
+            self.availability = False
+        super().save(*args, **kwargs)
+    """
+
+
 class CharField:
     """
-    par_1      || Descrição apresentada no template admin (rótulo do input)
-    help_text  || Texto de instrução p/ caso um campo não esteja claro o suficiente (pode descrever um exemplo)
-    max_length || Qtd. máxima de caracteres de uma string
+    max_length || Qtd. máxima de caracteres que o campo (caso seja texto) deve ter
     sintaxe    || models.CharField('Descrição', help_text='', max_length=100)
+    """
+
+
+class DateTimeField:
+    """
+    auto_now_add || Configura p/ que seja exibida os dados da criação do objeto ao modelo
+    auto_now     || Configura p/ que seja exibida os dados da última vez que o objeto sofreu alguma alteração
+    sintaxe      || models.DateTimeField('Data de criação', auto_now_add=True)
+    sintaxe      || models.DateTimeField('Última atualização', auto_now=True)
     """
 
 
@@ -13,8 +46,15 @@ class DecimalField:
     """
     par_1          || Descrição apresentada no template admin (rótulo do input)
     decimal_places || Qtd. de casa decimais após a vírgula
-    max_digits     || Qtd. máxima de dígitos permitidos antes de casa decimais
+    max_digits     || Qtd. máxima de dígitos permitidos antes das casa decimais
     sintaxe        || models.DecimalField('Descrição', decimal_places=2, max_digits=8)
+    """
+
+
+class FloatField:
+    """
+    par_1   || Descrição apresentada no template admin (rótulo do input)
+    sintaxe || models.FloatField('Descrição')
     """
 
 
@@ -59,17 +99,58 @@ class ImageField:
     """
 
 
-def integer_field():
+class IntegerField:
     """
-    par_1   || Descrição do que o número registrado será atrelado (ex: Número da casa)
+    par_1   || Descrição apresentada no template admin (rótulo do input)
     sintaxe || models.IntegerField('Descrição')
     """
 
 
 # MAIS INFOS: sintaxes_p_pa_models/relacionamentos/OneToOneField.py
-def one_to_one_field():
+class OneToOneField:
     """
-    par 1   || Especificar qual o modelo ao qual este campo pertence (pela lógica, o modelo alvo só deve ter este campo)
+    OBS     || Nessa contexto, temos 2 modelos envolvidos (relacionamento)
+    OBS     || O modelo servidor, apenas cria o campo que servirá o modelo receptor
+    OBS     || O modelo servidor cria o campo normalmente, enquanto o receptor recebe "models.OneToOneField"
+    par 1   || Especificar qual o modelo servidor (aquele que oferta o campo p/ o relacionamento)
     sintaxe || models.OneToOneField(Modelo, on_delete=models.CASCADE)
+
+    class Chassi(Base):
+        number = models.CharField('Número de série do veículo', help_text='17 caracteres' max_length=20)
+
+    class Vehicle(Base):
+        chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE)
     """
 
+
+class SlugField:
+    """
+    * Esse campo costuma ser configurado com os parâmetros "blank" e "null"
+    * Isso acontece, pois o campo de "slug" é uma forma interessante de obter um "url dinâmico"
+    * Supondo que um objeto seja um produto, ele possui os campos: name, slug
+    * name = "copo de plástico" ... slug = ''
+    * Uma função dentro do modelo que cria o objeto, pode ser configurada p/ passar o campo "name" p/ o campo "slug"
+    * RESULTADO: name = "copo de plástico" ... slug = "copo-de-plastico"
+    * ===== FUNÇÃO =====
+    * A biblioteca Python "uuid4" pode ajudar na complementação do slug
+
+    from django.utils.text import slugify
+    from uuid import uuid4
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.name) + f'-{uuid4()}'
+        super().save(*args, **kwargs)
+
+    blank   || Determina que o campo não possui nenhum valor (python o considera como "None")
+    null    || Determina que o campo não é acessível no template admin do Django
+    sintaxe || models.SlugField('Complemento', blank=True, null=True)
+    """
+
+
+class TextField:
+    """
+    OBS        || Idêntico ao "CharField", com exceção do tamanho do input (que é grande)
+    max_length || Qtd. máxima de caracteres que o campo (caso seja texto) deve ter
+    sintaxe    || models.CharField('Descrição', help_text='', max_length=100)
+    """
